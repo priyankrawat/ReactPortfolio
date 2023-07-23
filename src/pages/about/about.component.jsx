2193e77c4934e915ed34bd0a2cc342d5ffeeadfc
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2023-07-23 17:47:23 +0530

    Update about.component.jsx

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -11,7 +11,7 @@ const About = () => {
   return (
     <div id="about">
       <div className="about">
-        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME!</h1>
         <Container>
           <Row className="pt-3 pb-5 align-items-center">
             <Col xs={12} md={6}>
