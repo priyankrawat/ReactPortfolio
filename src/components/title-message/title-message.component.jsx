d259c10335291ae02118ca28a35a1cd1cd39c157
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2023-07-23 17:49:22 +0530

    Update title-message.component.jsx

--- src/components/title-message/title-message.component.jsx
+++ src/components/title-message/title-message.component.jsx
@@ -39,7 +39,7 @@ const TitleMessage = () => (
           Hi, I am
           <br />
           <span>
-            <strong>Priyank Rawat</strong>
+            <strong>Priyank Rawat, The DEV(eloper)</strong>
           </span>
         </div>
         <div className="sub">
