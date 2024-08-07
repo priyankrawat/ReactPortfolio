2ce860edeb5b401ee83dee9b70094d534a2b4914
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-08-07 09:22:33 +0530

    Update about.component.jsx

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -36,7 +36,7 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/1nayffuH1dFB1NLzAmkDT0KrtA7EIIcJb/view" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1xwVaFywuqudyrwMhn5DuAldL4RKhJ2hh/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
